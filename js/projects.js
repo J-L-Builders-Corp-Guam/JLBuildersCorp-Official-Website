008e929fa55705e777972a3055cdd0f488bf8f5b
Miguelle-Obispo <miguelleobispo@gmail.com>
2024-03-24 00:43:55 +0800

    savepoint projects

--- js/projects.js
+++ js/projects.js
@@ -1,18 +1,3 @@
-$(document).ready(function() {
-  // Hide all carousel and table elements initially
-  $('[id^="carousel-"]').hide();
-  $('[id^="table-"]').hide();
-  
-  // Show the corresponding carousel and table when a list-group item is clicked
-  $('.list-group-item').click(function() {
-    var targetId = $(this).attr('href').substring(1);
-    $('[id^="carousel-"]').hide();
-    $('[id^="table-"]').hide();
-    $('#carousel-' + targetId).show();
-    $('#table-' + targetId).show();
-  });
-});
-
 let header = document.querySelector('header');
 let logo = document.querySelector('.logo');
 let logomob = document.querySelector('.logomob');
@@ -49,68 +34,93 @@ window.onscroll = () => {
 };
 
 
-var links = document.querySelectorAll('.list-group-item');
+// once page is loaded, include the carousel and table
 
-// Add click event listener to each link
-links.forEach(function(link) {
-  link.addEventListener('click', function() {
-    // Remove 'activeAbout' class from all links
-    links.forEach(function(item) {
-      item.classList.remove('activeAbout');
-    });
-    
-    // Add 'activeAbout' class to the clicked link
-    this.classList.add('activeAbout');
-  });
-});
-
-// Get all the links in the list
-var links = document.querySelectorAll('.list-group-item');
-
-// Add click event listener to each link
-links.forEach(function(link) {
-  link.addEventListener('click', function() {
-    // Remove 'activeAbout' class from all tab panes
-    var tabPanes = document.querySelectorAll('.tab-pane');
-    tabPanes.forEach(function(pane) {
-      pane.classList.remove('show', 'activeAbout');
-    });
-    
-    // Add 'activeAbout' class to the clicked link
-    this.classList.add('activeAbout');
-    
-    // Get the target tab pane and add 'show' and 'activeAbout' classes
-    var targetPaneId = this.getAttribute('href').substring(1); // Remove '#'
-    var targetPane = document.getElementById(targetPaneId);
-    targetPane.classList.add('show', 'activeAbout');
-  });
-});
-
-document.addEventListener('DOMContentLoaded', function() {
+$(document).ready(function() {
+  // Hide all carousel and table elements initially
+  $('[id^="carousel-"]').hide();
+  $('[id^="table-"]').hide();
+  
   // Get the hash from the URL
   var hash = window.location.hash;
 
   // If a hash exists
   if (hash) {
-      // Remove 'active' class from all list items
-      var listItems = document.querySelectorAll('.list-group-item');
-      listItems.forEach(function(item) {
-          item.classList.remove('activeAbout');
-      });
+    // Remove 'active' class from all list items
+    $('.list-group-item').removeClass('activeAbout');
 
-      // Remove 'active' class from all tab panes
-      var tabPanes = document.querySelectorAll('.tab-pane');
-      tabPanes.forEach(function(pane) {
-          pane.classList.remove('show', 'activeAbout');
-      });
+    // Remove 'active' class from all tab panes
+    $('.tab-pane').removeClass('show activeAbout');
 
-      // Add 'active' class to the corresponding list item
-      var listItem = document.querySelector('.list-group-item[href="' + hash + '"]');
-      listItem.classList.add('activeAbout');
+    // Add 'active' class to the corresponding list item
+    var listItem = $('.list-group-item[href="' + hash + '"]');
+    listItem.addClass('activeAbout');
 
-      // Add 'show' and 'active' classes to the corresponding tab pane
-      var tabPane = document.querySelector('.tab-pane' + hash);
-      tabPane.classList.add('show', 'activeAbout');
+    // Add 'show' and 'active' classes to the corresponding tab pane
+    var tabPane = $(hash);
+    tabPane.addClass('show activeAbout');
+
+    // Show the corresponding carousel and table
+    var targetId = hash.substring(1);
+    $('#carousel-' + targetId).show();
+    $('#table-' + targetId).show();
+  } else {
+    // If no hash exists, set default project page as active
+    var defaultProjectPage = $('#list-profile-list'); // Adjust this selector to target your default project page link
+    defaultProjectPage.addClass('activeAbout');
+
+    // Add 'show' and 'active' classes to the corresponding tab pane
+    var defaultTabPane = $('#commercial'); // Adjust this selector to target your default tab pane
+    defaultTabPane.addClass('show activeAbout');
+
+    // Show the corresponding carousel and table for the default project page
+    $('#carousel-commercial').show();
+    $('#table-commercial').show();
   }
+
+  // Show the corresponding carousel and table when a list-group item is clicked
+  $('.list-group-item').click(function() {
+    var targetId = $(this).attr('href').substring(1);
+
+    // Remove 'active' class from all list items
+    $('.list-group-item').removeClass('activeAbout');
+
+    // Remove 'active' class from all tab panes
+    $('.tab-pane').removeClass('show activeAbout');
+
+    // Add 'active' class to the clicked list item
+    $(this).addClass('activeAbout');
+
+    // Add 'show' and 'active' classes to the corresponding tab pane
+    $('#' + targetId).addClass('show activeAbout');
+
+    // Hide all carousel and table elements initially
+    $('[id^="carousel-"]').hide();
+    $('[id^="table-"]').hide();
+
+    // Show the corresponding carousel and table
+    $('#carousel-' + targetId).show();
+    $('#table-' + targetId).show();
+  });
+});
+
+
+// swiperjs
+var swiper = new Swiper('.mySwiper', {
+  effect: 'coverflow',
+  grabCursor: true,
+  centeredSlides: true,
+  slidesPerView: 'auto',
+  coverflowEffect: {
+    rotate: 50,
+    stretch: 0,
+    depth: 100,
+    modifier: 1,
+    slideShadows: true,
+  },
+  pagination: {
+    el: '.swiper-pagination',
+    clickable: true,
+  },
 });
 
