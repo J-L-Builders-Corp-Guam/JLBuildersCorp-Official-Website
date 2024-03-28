98884bd911b5b2a7218a22f5adb63f510e4de992
Miguelle-Obispo <miguelleobispo@gmail.com>
2024-03-29 02:26:46 +0800

    email js temp email meg

--- js/script.js
+++ js/script.js
@@ -85,30 +85,38 @@ function sendemail(event) {
   var validmail = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
   var validnumber = /^\+?\d+$/;
 
+  var errors = [];
+
   if (thename == "") {
-    alert("Please Enter Name");
-  } else if (themail == "" || !themail.match(validmail)) {
-    alert("Please Enter Valid Email");
-  } else if (!thenumber.match(validnumber)) {
-    alert("Please Enter a Valid Number");
-  } else if (themsg == "") {
-    alert("Please Enter Message");
+    errors.push("Please Enter Name");
+  } 
+  if (themail == "" || !themail.match(validmail)) {
+    errors.push("Please Enter Valid Email");
+  } 
+  if (!thenumber.match(validnumber)) {
+    errors.push("Please Enter a Valid Number");
+  }
+  if (themsg == "") {
+    errors.push("Please Enter Message");
+  }
+  if (errors.length > 0) {
+    alert(errors.join("\n"));
   } else {
     var contactdetail = {
       to_name: 'J&L Builders Team',
       from_name: thename,
       from_email: themail,
-      message: thenumber + ' ' + themsg
+      message: 'contact number of the sender is: \n' + thenumber + '\n' + themsg
     };
 
     emailjs.send('service_qnnyqnq', 'template_cepqsnk', contactdetail).then(
       (response) => {
         console.log("SUCCESS!", response.status, response.text);
-        alert("SUCCESS!");
+        alert("Message Sent!");
       },
       (error) => {
         console.log("FAILED...", error);
-        alert("FAILED...");
+        alert("Error...");
       }
     );
   }
@@ -116,97 +124,50 @@ function sendemail(event) {
 
   
 
-function sendemail2() {
-  emailjs.init({
-    publicKey: 'CtvMIckQX4OtBchLC',
-  });
+function sendemail2(event) {
+  event.preventDefault();
 
   var thename2 = document.getElementById('name2').value;
   var themail2 = document.getElementById('email2').value;
   var thenumber2 = document.getElementById('number2').value;
   var themsg2 = document.getElementById('msg2').value;
   var validmail2 = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
-  
+  var validnumber2 = /^\+?\d+$/;
+
+  var errors2 = [];
+
   if (thename2 == "") {
-    alert("Please Enter Name");
+    errors2.push("Please Enter Name");
+  } 
+  if (themail2 == "" || !themail2.match(validmail2)) {
+    errors2.push("Please Enter Valid Email");
+  } 
+  if (!thenumber2.match(validnumber2)) {
+    errors2.push("Please Enter a Valid Number");
   }
-  else if (themail2 == "" || !themail2.match(validmail2)) {
-    alert("Please Enter Valid Email");
+  if (themsg2 == "") {
+    errors2.push("Please Enter Message");
   }
-  else if (!(/^\+?\d+$/.test(thenumber2))) {
-    alert("Please Enter a Valid Number");
-  }
-  else if (themsg2 == "") {
-    alert("Please Enter Message");
+  if (errors2.length > 0) {
+    alert(errors2.join("\n"));
   } else {
     var contactdetail2 = {
       to_name: 'J&L Builders Team',
       from_name: thename2,
       from_email: themail2,
-      message: thenumber2 + ' ' + themsg2 
+      message: 'contact number of the sender is: \n' + thenumber2 + '\n' + themsg2
     };
-    
-    emailjs.send('service_qnnyqnq', 'template_cepqsnk', contactdetail2).then(function (res) {
-      alert("Email Sent Successfully");
-    }, function(reason) {
-      alert("Error Occurred: " + reason);
-    });
+
+    emailjs.send('service_qnnyqnq', 'template_cepqsnk', contactdetail2).then(
+      (response) => {
+        console.log("SUCCESS!", response.status, response.text);
+        alert("Message Sent!");
+      },
+      (error) => {
+        console.log("FAILED...", error);
+        alert("Error...");
+      }
+    );
   }
-}
+};
 
-// test button fx
-// function sendemail() {
-//   var thename = document.getElementById('name').value;
-//   var themail = document.getElementById('email').value;
-//   var thenumber = document.getElementById('number').value;
-//   var themsg = document.getElementById('msg').value;
-//   var validmail = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
-  
-//   if (thename == "") {
-//     alert("Please Enter Name");
-//   }
-//   else if (themail == "" || !themail.match(validmail)) {
-//     alert("Please Enter Valid Email");
-//   }
-//   else if (!(/^\+?\d+$/.test(thenumber))) {
-//     alert("Please Enter a Valid Number");
-//   }
-//   else if (themsg == "") {
-//     alert("Please Enter Message");
-//   }
-//   else {
-//     console.log("Name:", thename);
-//     console.log("Email:", themail);
-//     console.log("Number:", thenumber);
-//     console.log("Message:", themsg);
-//     alert("Email Logged to Console");
-//   }
-// }
-
-// function sendemail2() {
-//   var thename2 = document.getElementById('name2').value;
-//   var themail2 = document.getElementById('email2').value;
-//   var thenumber2 = document.getElementById('number2').value;
-//   var themsg2 = document.getElementById('msg2').value;
-//   var validmail2 = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
-  
-//   if (thename2 == "") {
-//     alert("Please Enter Name");
-//   }
-//   else if (themail2 == "" || !themail2.match(validmail2)) {
-//     alert("Please Enter Valid Email");
-//   }
-//   else if (!(/^\+?\d+$/.test(thenumber2))) {
-//     alert("Please Enter a Valid Number");
-//   }
-//   else if (themsg2 == "") {
-//     alert("Please Enter Message");
-//   }
-//   else {
-//     console.log("Name:", thename2);
-//     console.log("Email:", themail2);
-//     console.log("Number:", thenumber2);
-//     console.log("Message:", themsg2);
-//     alert("Email Logged to Console");
-//   }
-// }
