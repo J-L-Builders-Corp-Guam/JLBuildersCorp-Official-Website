e99e8c99518a59bc8d4bf259b9cb3183971c6e76
Miguelle-Obispo <miguelleobispo@gmail.com>
2024-04-12 18:12:44 +0800

    lazy loading img gallery

--- js/gallery.js
+++ js/gallery.js
@@ -1,3 +1,32 @@
+// lazy load
+window.addEventListener("scroll", lazyLoad);
+
+function lazyLoad() {
+    const images = document.querySelectorAll("img[data-src]");
+    
+    images.forEach(img => {
+        if (isInViewport(img)) {
+            img.src = img.dataset.src;
+            img.removeAttribute("data-src");
+        }
+    });
+}
+
+function isInViewport(element) {
+    const rect = element.getBoundingClientRect();
+    return (
+        rect.top >= 0 &&
+        rect.left >= 0 &&
+        rect.bottom <= (window.innerHeight || document.documentElement.clientHeight) &&
+        rect.right <= (window.innerWidth || document.documentElement.clientWidth)
+    );
+}
+
+
+
+
+
+
 // lightbox
 
 window.addEventListener("load", () => {
